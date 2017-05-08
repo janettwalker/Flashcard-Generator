50664ec7b239c22a03ee4fa8f4b1be055cce4cab
Janet Walker <janetwalker271989@gmail.com>
2017-05-08 14:36:43 -0400

    Set up constructors and functionality for flashcards

--- BasicCard.js
+++ BasicCard.js
@@ -1,8 +1,23 @@
 var BasicCard = function(front, back){
 	this.front = front;
-	this.back = front;
+	this.back = back;
 
 };
+
+
+BasicCard.prototype.printCard = function() {
+    console.log('Front: ' + this.front + ', ' + 'Back: ' + this.back);
+};
+
+BasicCard.prototype.printFront = function() {
+    console.log(this.front);
+};
+
+
+BasicCard.prototype.printAnswer = function() {
+    console.log('Sorry, the correct answer is ' + this.back + '.');
+};
+
 
 //Node module that exports a constructor for creating basic flashcards
 module.exports = BasicCard;
--- ClozeCard.js
+++ ClozeCard.js
@@ -1,5 +1,22 @@
-var CloseCard = function(text, cloze){
+var ClozeCard = function(text, cloze){
+
+    this.text = text;
+    this.cloze = this.text.match(/\(([^)]+)\)/)[1];
+    this.printCloze = function() {
+        console.log(this.cloze);
+    }
+    this.printText = function() {
+        console.log(this.text);
+    }
+
+    this.message = this.text.replace('(' + this.cloze + ')', '________');
 
 };
 
-module.exports = CloseCard;
+ClozeCard.prototype.printAnswer = function() {
+
+    console.log('Incorrect. Here is the full sentence: \n' + this.text.replace(/[{()}]/g, ''));
+}
+
+//Node module that exports cloze deletion flashcards
+module.exports = ClozeCard;
